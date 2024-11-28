5837056ceae28ace38c4fab0157f6bdb2155c565
Anamika M S <24mp2278@rit.ac.in>
2024-11-28 10:14:03 +0530

    Create Experiment 5.py

---
+++ Cycle 1/Experiment 5.py
@@ -0,0 +1,9 @@
+# Aim : Program to perform arithmetic operations on 2 integers
+
+num1=int(input("Enter first number :"))
+num2=int(input("Enter second number :"))
+print(f"Addition : {num1+num2}")
+print(f"Substraction : {num1-num2}")
+print(f"Multiplication :{num1*num2}")
+print(f"Division :{num1/num2}")
+print(f"Modulus :{num1%num2}")
