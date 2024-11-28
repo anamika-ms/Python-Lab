b5c515d805e1e491918331de5da6b94627295bcb
Anamika M S <24mp2278@rit.ac.in>
2024-11-28 10:14:49 +0530

    Create Experiment 6.py

---
+++ Cycle 1/Experiment 6.py
@@ -0,0 +1,6 @@
+# Aim : Program to print n copies of a given string
+
+string=input("Enter a string:")
+n=int(input("Enter a non negative integer:"))
+result= string*n
+print(f"The result : {result}")
