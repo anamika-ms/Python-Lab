34676dcc9525b05d5e041466aa81a5acc1ab75a1
Anamika M S <24mp2278@rit.ac.in>
2024-11-28 10:12:32 +0530

    Create Experiment 3.py

---
+++ Cycle 1/Experiment 3.py
@@ -0,0 +1,5 @@
+# Aim : Program to calculate area of circle
+
+radius=float(input("Enter the radius of the circle :"))
+area=3.14*radius**2
+print(f"The area of circle with radius {radius} is {area}")
