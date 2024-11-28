308dc621ad7d1c87bf8a417ad33f6d22251a0105
Anamika M S <24mp2278@rit.ac.in>
2024-11-28 10:07:35 +0530

    Create Experiment_1.py

---
+++ Cycle 1/Experiment_1.py
@@ -0,0 +1,5 @@
+# Aim : Program to print greetings message
+
+first_name=input("Enter your first name :")
+second_name=input("Enter your second name :")
+print(f"Greetings !!! {first_name} {second_name}")
