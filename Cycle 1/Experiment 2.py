7960f847b1e3d28825202beab7b9ce32607fe572
Anamika M S <24mp2278@rit.ac.in>
2024-11-28 10:11:52 +0530

    Create Experiment 2.py

---
+++ Cycle 1/Experiment 2.py
@@ -0,0 +1,8 @@
+# Aim : Program to demonstrate diffrent number datatypes
+
+int_num=10
+float_num=10.5
+complex_num=2+3j
+print(f"Integer : {int_num} , Type : {type(int_num)}")
+print(f"Float : {float_num} , Type : {type(float_num)}")
+print(f"Complex : {complex_num} , Type : {type(complex_num)}")
