85bbe9e326e97b2bd28a2ae1042bde0ef8323f8b
Anamika M S <24mp2278@rit.ac.in>
2024-11-29 13:47:37 +0530

    Update Experiment 3.py

--- Cycle 1/Experiment 3.py
+++ Cycle 1/Experiment 3.py
@@ -1,5 +1,15 @@
-# Aim : Program to calculate area of circle
+# Aim: 
+# Write a program to calculate the area of a circle by reading inputs from the user.
+
+# Pseudocode:
+# 1. Read r.
+# 2. Calculate area= π*r*r .
+# 3. Print area.
 
 radius=float(input("Enter the radius of the circle :"))
 area=3.14*radius**2
 print(f"The area of circle with radius {radius} is {area}")
+
+# Output:
+# Enter the radius of circle:2
+# The area of circle is 12.56
