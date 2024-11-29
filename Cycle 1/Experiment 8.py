758e99772addd12d5f6a3144bafd0d62992cfc6a
Anamika M S <24mp2278@rit.ac.in>
2024-11-29 13:53:33 +0530

    Update Experiment 8.py

--- Cycle 1/Experiment 8.py
+++ Cycle 1/Experiment 8.py
@@ -1,11 +1,24 @@
-# Aim : Program to find biggest of 3 numbers
+# Aim: 
+# Find biggest of 3 numbers entered.
+
+# Pseudocode:
+# 1. Read num1,num2,num3.
+# 2. If num1>=num2 and num1>=num2, Print num1 is largest.
+# 3. Else if num2>=num1 and num2>=num3, Print num2 is largest.
+# 4. Else, Print num3 is largest.
 
 num1=int(input("Enter first number :"))
 num2=int(input("Enter second number :"))
 num3=int(input("Enter third number :"))
 if num1>num2 & num1>num3:
-print(f"{num1} is biggest.")
+  print(f"{num1} is biggest.")
 elif num2>num3:
-print(f"{num2} is biggest.")
+  print(f"{num2} is biggest.")
 else:
-print(f"{num3} is biggest.")
+  print(f"{num3} is biggest.")
+
+# Output:
+# Enter first number:15
+# Enter second number:25
+# Enter third number:35
+# 35 is biggest number
