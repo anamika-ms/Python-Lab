2a50ff0dc8ca30871f56b99cdfdbdd81e80ff93e
Anamika M S <24mp2278@rit.ac.in>
2024-11-29 13:45:21 +0530

    Update Experiment 1.py

--- Cycle 1/Experiment 1.py
+++ Cycle 1/Experiment 1.py
@@ -1,5 +1,16 @@
-# Aim : Program to print greetings message.
+# Aim: 
+# Write a program that prompts the user to enter his first name and last name and then displays a message “Greetings!!! First name Last name”.
+
+# Pseudocode:
+# 1. Read first name.
+# 2. Read last name.
+# 3. Print Greetings!!! First name, Last name.
 
 first_name=input("Enter your first name :")
 second_name=input("Enter your second name :")
 print(f"Greetings !!! {first_name} {second_name}")
+
+# Output:
+# Enter the first name:Anamika
+# Enter the last name:Shaji
+# Greetings!!!Anamika Shaji
