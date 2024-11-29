486963fbbb4f7b1c212e388b4e669f6f4bbd7d1b
Anamika M S <24mp2278@rit.ac.in>
2024-11-29 13:50:26 +0530

    Update Experiment 5.py

--- Cycle 1/Experiment 5.py
+++ Cycle 1/Experiment 5.py
@@ -1,4 +1,13 @@
-# Aim : Program to perform arithmetic operations on 2 integers
+# Aim: 
+# Write a Python program to perform arithmetic operations on two integer numbers.
+
+# Pseudocode:
+# 1. Read num1,num2.
+# 2. Perform and print Addition: num1 + num2.
+# 3. Perform and print Subtraction: num1 - num2.
+# 4. Perform and print Multiplication: num1 * num2.
+# 5. Perform and print Division: num1 / num2.
+# 6. Perform and print Modulus: num1 % num2.
 
 num1=int(input("Enter first number :"))
 num2=int(input("Enter second number :"))
@@ -7,3 +16,12 @@ print(f"Substraction : {num1-num2}")
 print(f"Multiplication :{num1*num2}")
 print(f"Division :{num1/num2}")
 print(f"Modulus :{num1%num2}")
+
+# Output:
+# Enter the first integer:2
+# Enter the second integer:3
+# Addition:5
+# Subtraction:-1
+# Multiplication:6
+# Division:0.6666666666666
+# Modulus:2
