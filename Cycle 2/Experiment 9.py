654e589421ddaf45c82173fe39b4c16ef9e46e3d
Anamika M S <24mp2278@rit.ac.in>
2024-11-29 13:27:16 +0530

    Create Experiment 9.py

---
+++ Cycle 2/Experiment 9.py
@@ -0,0 +1,21 @@
+# Aim: 
+# Program to prompt the user for a list of integers ,for all values greater than 100 store ‘over’ instead.
+
+# Pseudocode:
+# 1. Read num.
+# 2. If num >100 , Set num=over.
+# 3. Add num to list.
+# 4. Print list.
+
+n=int(input("Enter number of integers to input:"))
+list1=[]
+for i in range(n):
+  num=int(input("Enter integer:"))
+  if num>100:
+    num="over"
+  list1.append(num)
+print(list1)
+
+# Output:
+# Enter number of integers to input : 3
+# Enter integers:46
