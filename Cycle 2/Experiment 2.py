470435ed97da8601f78d0ef8883eb5cd848215ca
Anamika M S <24mp2278@rit.ac.in>
2024-11-29 13:13:05 +0530

    Update Experiment 2.py

--- Cycle 2/Experiment 2.py
+++ Cycle 2/Experiment 2.py
@@ -1,22 +1,19 @@
-""" 
-Aim:
-Program to get a string from an input string where all occurences of first
-character are replaced with ‘$’,except for first character.
+# Aim:
+# Program to get a string from an input string where all occurences of first
+# character are replaced with ‘$’,except for first character.
 
-Pseudocode:
-1. Read string.
-2. first_char=s[0].
-3. newstr=first_char +s[1:].replace(f, ‘$’).
-4. Print newstr. 
-"""
+# Pseudocode:
+# 1. Read string.
+# 2. first_char=s[0].
+# 3. newstr=first_char +s[1:].replace(f, ‘$’).
+# 4. Print newstr. 
 
 s=input("Entre a string :")
 first_char=s[0]
 newstr= first_char +s[1:].replace(f, ‘$’)
 print(“{newstr}”)
 
-"""
-Output:
-Enter a string: onion
-Output: oni$n
-"""
+# Output:
+# Enter a string: onion
+# Output: oni$n
+
