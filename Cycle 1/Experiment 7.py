28f33267fda6b73bd9fd9f3400440df6a84b247b
Anamika M S <24mp2278@rit.ac.in>
2024-11-29 13:52:21 +0530

    Update Experiment 7.py

--- Cycle 1/Experiment 7.py
+++ Cycle 1/Experiment 7.py
@@ -1,5 +1,15 @@
-# Aim : Program to accept an integer and compute n+nn+nnn
+# Aim: 
+# Accept an integer n and compute n+nn+nnn.
+
+# Pseudocode:
+# 1. Read n.
+# 2. Calculate result= n+(n*10+n)+(n*100+n*10+n).
+# 3. Print result.
 
 n=int(input("Enter a integer :"))
 result=n+(n*10+n)+(n*100+n*10+n)
 print(f"Result :{n}+{n}{n}+{n}{n}{n}={result}")
+
+# Output:
+# Enter your n: 4
+# Result:4+44+444=492
