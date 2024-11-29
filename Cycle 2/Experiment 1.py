9ac7bb92df57b9595ba327198066417133168bc9
Anamika M S <24mp2278@rit.ac.in>
2024-11-29 13:13:36 +0530

    Update Experiment 1.py

--- Cycle 2/Experiment 1.py
+++ Cycle 2/Experiment 1.py
@@ -1,18 +1,15 @@
-""" 
-Aim: 
-Create a string from the given string where the first and last character are exchanged.
+# Aim: 
+# Create a string from the given string where the first and last character are exchanged.
 
-Pseudocode:
-1. Read str.
-2. newstr=str[-1]+str[1:-1]+str[0].
-3. Print newstr.
-"""
+# Pseudocode:
+# 1. Read str.
+# 2. newstr=str[-1]+str[1:-1]+str[0].
+# 3. Print newstr.
 
 str="python"
 newstr=str[-1]+str[1:-1]+str[0]
 print(newstr)
 
-"""
-Output:
-nythop
-"""
+# Output:
+# nythop
+
