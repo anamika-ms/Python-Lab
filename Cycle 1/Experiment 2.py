abfe773ffc1251d3e8e0595755154c9528e3ea41
Anamika M S <24mp2278@rit.ac.in>
2024-11-29 13:46:30 +0530

    Update Experiment 2.py

--- Cycle 1/Experiment 2.py
+++ Cycle 1/Experiment 2.py
@@ -1,4 +1,9 @@
-# Aim : Program to demonstrate diffrent number datatypes
+# Aim: 
+# Write a program to demonstrate different number data types in python.
+
+# Pseudocode:
+# 1. Assign int_num=2, float_num=2.44,complex_num=2+4j.
+# 2. Print type of int_num, float_num, complex_num.
 
 int_num=10
 float_num=10.5
@@ -6,3 +11,8 @@ complex_num=2+3j
 print(f"Integer : {int_num} , Type : {type(int_num)}")
 print(f"Float : {float_num} , Type : {type(float_num)}")
 print(f"Complex : {complex_num} , Type : {type(complex_num)}")
+
+# Output:
+# Integer:2 Type:<class ‘int’>
+# Float:2.44 Type:<class ‘float’>
+# Complex:(2+4j) Type:<class ‘complex’>
