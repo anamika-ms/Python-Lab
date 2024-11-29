74700aa8aad692cf0fca676827c9aa23f7d42a33
Anamika M S <24mp2278@rit.ac.in>
2024-11-29 13:51:20 +0530

    Update Experiment 6.py

--- Cycle 1/Experiment 6.py
+++ Cycle 1/Experiment 6.py
@@ -1,6 +1,18 @@
-# Aim : Program to print n copies of a given string
+# Aim: 
+# Write a Python program to get a string which is n (non-negative integer) copies
+# of a given string.
+
+# Pseudocode:
+# 1. Read string.
+# 2. Read number of repetition , n.
+# 3. Print string*n.
 
 string=input("Enter a string:")
 n=int(input("Enter a non negative integer:"))
 result= string*n
 print(f"The result : {result}")
+
+# Output:
+# Enter a string: anu
+# Enter a non-negative integer:4
+# Result: anuanuanuanu
