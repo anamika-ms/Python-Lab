a51f5f7d16e79fb8501750ba9961092de3b4f36c
Anamika M S <24mp2278@rit.ac.in>
2024-11-29 13:56:03 +0530

    Update Experiment 10.py

--- Cycle 1/Experiment 10.py
+++ Cycle 1/Experiment 10.py
@@ -1,10 +1,21 @@
-# Aim : Program to determine the rate of entry ticket in a trade fair based on age.
+# Aim: 
+# Program to determine the rate of entry ticket in a trade fair based on age.
+
+# Pseudocode:
+# 1. Read age .
+# 2. if age is less than 10 , Print Ticket rate 7 .
+# 3. Else, if age is less than 60, Print Ticket rate 10.
+# 4. Else , Print Ticket rate 5.
 
 age=int(input("Enter your age :"))
 if age<10:
-rate=7
+  rate=7
 elif age>=10 and age<60:
-rate=10
+  rate=10
 else:
-rate=5
+  rate=5
 print(f"The rate of entry ticket is {rate}.")
+
+# Output:
+# Enter your age :25
+# The rate of entry ticket is 7
