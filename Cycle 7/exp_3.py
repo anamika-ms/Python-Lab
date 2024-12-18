1f0db33ad5af82eae2f6aeb19e40dc817ad9056e
Anamika M S <24mp2278@rit.ac.in>
2024-12-18 15:07:57 +0530

    Add files via upload

---
+++ Cycle 7/exp_3.py
@@ -0,0 +1,11 @@
+import csv
+
+def read_csv_file(filename):
+    with open(filename, mode='r', newline='') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            print(row)
+
+
+csv_filename = "sample.csv"
+read_csv_file(csv_filename)
